72f26e874dbe12657e35f028b4a5b3539588fcc5
Avihai Shalom <avihais12344@gmail.com>
2022-07-06 12:30:08 +0300

    Finished the fourth exercise.

--- src/ex-4/main.c
+++ src/ex-4/main.c
@@ -7,6 +7,15 @@ void print_rectangle(const Rectangle* rect){
 
 int main(void)
 {
-    Rectangle rectangle1 = {5, 6};
-    print_rectangle(&rectangle1);
+    Rectangle rect1 = {5, 6};
+    Rectangle rect2 = {12, 12};
+    printf("Rectangle 1 = ");
+    print_rectangle(&rect1);
+    printf("Rectangle 2 = ");
+    print_rectangle(&rect2);
+    printf("Area of the rectangle 1: %f\n", calculate_area_of_rectangle(&rect1));
+    printf("Area of the rectangle 2: %f\n", calculate_area_of_rectangle(&rect2));
+    printf("Rectangle with the bigger area: %s\n", get_rectangle_with_bigger_area(&rect1, &rect2) == &rect1 ? "Rectangle 1" : "Rectangle 2");
+    printf("Can rectangle 2 contain rectangle 1: %s\n", can_rectangle_contain_rectangle(&rect2, &rect1) ? "True" : "False");
+    printf("Can rectangle 1 contain rectangle 2: %s\n", can_rectangle_contain_rectangle(&rect1, &rect2) ? "True" : "False");
 }
